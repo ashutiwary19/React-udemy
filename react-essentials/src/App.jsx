dfaec281330463c9036de6cf417c581a43d800cb
Ashu Tiwary <ashu_tiwary@Ashus-MacBook-Air.local>
2026-02-04 02:21:28 +0530

    Added event listener

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -2,9 +2,15 @@ import reactImg from './assets/react-core-concepts.png';
 import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = <p>Select a tab to see an example.</p>;
+  function handleSelect(selectedButton) {
+    // Selected button => JSX, Components, Props, State
+    console.log('Tab selected:', selectedButton);
+    tabContent = <p>You selected the {selectedButton} tab.</p>;
+  }
   return (  
     <div>
      <Header />
@@ -20,7 +26,16 @@ function App() {
             {/* You can add more <CoreConcept /> components here for additional concepts */}
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+            </menu>
+            {tabContent}
+        </section>
       </main>
     </div>
   );
