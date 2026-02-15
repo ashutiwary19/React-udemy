0b9da0a5cd7c2548813473c5422b88be87bd7766
Ashu Tiwary <ashu.tiwary19@gmail.com>
2026-02-16 03:42:15 +0530

    CSS Rules scoping

--- React Styling/src/components/Header.jsx
+++ React Styling/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
+// import "./Header.css";
+import classes from "./Header.module.css";
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p
-      /* style={{
+        className={classes.paragraph}
+        /* style={{
           color: "#af2121",
           textAlign: "center",
         }} */
