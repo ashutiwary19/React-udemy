352bb21a6d45a726e2b923ae404485800682a61f
Ashu Tiwary <ashu.tiwary19@gmail.com>
2026-02-18 01:44:20 +0530

    Added useRef to get a field value on a btn click

--- RefsAndPortals/src/components/Player.jsx
+++ RefsAndPortals/src/components/Player.jsx
@@ -1,21 +1,28 @@
 import { useState, useRef } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState("");
+  const inputRef = useRef();
+  // const [playerName, setPlayerName] = useState("");
   const [nameSubmitted, setNameSubmitted] = useState("");
-  function handleChange(e) {
+  /* function handleChange(e) {
     setPlayerName(e.target.value);
-  }
+  } */
 
   function handleSubmit() {
-    setNameSubmitted(playerName);
+    // setNameSubmitted(playerName);
+    setNameSubmitted(inputRef.current.value);
   }
 
   return (
     <section id="player">
       <h2>Welcome {nameSubmitted}</h2>
       <p>
-        <input type="text" value={playerName} onChange={handleChange} />
+        <input
+          ref={inputRef}
+          type="text"
+          // value={playerName}
+          // onChange={handleChange}
+        />
         <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
