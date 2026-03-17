8b15baf26d819e29d2a8f5315dcd07ada9a2808b
Ashu Tiwary <ashu.tiwary19@gmail.com>
2026-03-17 20:05:34 +0530

    Added useeffect to get data from server

---
+++ Http_requests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Places from "./Places.jsx";
+import { useEffect } from "react";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  // Problem with this code is every time data is fetched the component will get rerendered
+  // and the fetch api is called again.
+  // So the component will create an infinite loop
+  /*fetch("http://localhost:3000/places")
+    .then((placesResponse) => placesResponse.json())
+    .then((resData) => setAvailablePlaces(resData.places));*/
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((placesResponse) => placesResponse.json())
+      .then((resData) => setAvailablePlaces(resData.places));
+  }, []);
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
