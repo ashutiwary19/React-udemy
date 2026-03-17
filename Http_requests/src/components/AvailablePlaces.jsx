8d6c4738a557ea00ba7359a0b958860a45950ef6
Ashu Tiwary <ashu.tiwary19@gmail.com>
2026-03-17 20:35:29 +0530

    Added error handling

--- Http_requests/src/components/AvailablePlaces.jsx
+++ Http_requests/src/components/AvailablePlaces.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Places from "./Places.jsx";
 import { useEffect } from "react";
+import ErrorPage from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   // Problem with this code is every time data is fetched the component will get rerendered
   // and the fetch api is called again.
   // So the component will create an infinite loop
@@ -15,11 +18,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
     /* fetch("http://localhost:3000/places")
       .then((placesResponse) => placesResponse.json())
       .then((resData) => setAvailablePlaces(resData.places));*/
+    setIsLoading(true);
     fetchPlaces();
     async function fetchPlaces() {
-      const resposne = await fetch("http://localhost:3000/places");
-      const responseJson = await resposne.json();
-      setAvailablePlaces(responseJson.places);
+      try {
+        const resposne = await fetch("http://localhost:3000/places");
+        const responseJson = await resposne.json();
+        if (!resposne.ok) {
+          throw new Error("Error while fetching places");
+        }
+        setAvailablePlaces(responseJson.places);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     /* const placePromise = fetchPlaces();
     placePromise.then((places) => setAvailablePlaces(places));
@@ -29,10 +42,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
       return responseJson.places;
     }*/
   }, []);
+  if (error) {
+    return <ErrorPage title="An Error occured!" message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
