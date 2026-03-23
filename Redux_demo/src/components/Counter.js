bc958ba20bb0d384ce1e489170c7d635684003e6
Ashu Tiwary <ashu.tiwary19@gmail.com>
2026-03-24 00:51:49 +0530

    Added showCounter state - multiple states

--- Redux_demo/src/components/Counter.js
+++ Redux_demo/src/components/Counter.js
@@ -2,9 +2,8 @@ import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
   const counter = useSelector((state) => state.counter);
-
+  const showCounter = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
@@ -17,16 +16,20 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
+  const toogleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment By 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toogleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
--- Redux_demo/src/store/index.js
+++ Redux_demo/src/store/index.js
@@ -1,15 +1,24 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + (action?.amount ?? 1),
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
     };
   }
 
