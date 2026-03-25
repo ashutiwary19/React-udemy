38a1a311124373e049c6c4e5268bb4877334d4c3
Ashu Tiwary <ashu.tiwary19@gmail.com>
2026-03-26 00:18:20 +0530

    Added dummy backend

---
+++ Backend_dummy/Dummy/src/main/java/ashu/react/demo/DummyApplication.java
@@ -0,0 +1,13 @@
+package ashu.react.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyApplication.class, args);
+	}
+
+}
---
+++ Backend_dummy/Dummy/src/test/java/ashu/react/demo/DummyApplicationTests.java
@@ -0,0 +1,13 @@
+package ashu.react.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
